27db8e06f7ffb46fc7f1d73bf38b444125bf4908
Bhoj Sao <bhojsao@ymail.com>
2019-09-30 21:09:21 +0530

    Added Eureka Configuration

--- src/main/java/com/bhoj/springboot/SpringBootFlywayApp.java
+++ src/main/java/com/bhoj/springboot/SpringBootFlywayApp.java
@@ -2,6 +2,7 @@ package com.bhoj.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Spring Boot Flyway App Main Class
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication(scanBasePackages = { "com.bhoj.springboot" })
+@EnableEurekaClient
 public class SpringBootFlywayApp {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootFlywayApp.class, args);
